f87f59798f06202907a586e1df5a3976f92286c1
ricardocaif-eb <ricardo.caif@eventbrite.com>
2021-11-13 09:39:41 -0300

    Updated eb dotfiles config

--- tmuxconf/weather.py
+++ tmuxconf/weather.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 URL = 'https://www.meteored.com.ar/tiempo-en_Mendoza-America+Sur-Argentina-Mendoza-SAME-1-16887.html'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
-clima = soup.find('span', class_='estado').find('strong').text.strip()
+clima = soup.find('span', class_='descripcion').find('strong').text.strip()
 temperatura = soup.find('span', class_='dato-temperatura').text.strip()
 
 print(u'{clima} {temperatura}'.format(clima=clima, temperatura=temperatura))
